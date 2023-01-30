a084a91abaf4d5c496da1e3afc246087170c4c4f
Also Ailton <ailton_gulmanelli@hotmail.com>
2023-01-30 11:21:22 -0300

    food app stackup in react

---
+++ src/recipe.js
@@ -0,0 +1,39 @@
+export const my_recipe = {
+    recipe_1: {
+      name: 'Chirashi Don',
+      preparation_time: '30 minutes',
+      servings: '2',
+      img : "/img/recipe1.jpg",
+      ingredients: [
+        '1/2 cup rice',
+        '1/2 cup water',
+        '2 eggs',
+        '300g of salmon, tuna and salmon roe',
+      ],
+      instructions: [
+        'Cook rice and water in a rice cooker',
+        'Fry eggs in a pan',
+        'Serve rice, eggs and fish on a plate',
+    
+      ],
+   
+    },
+    recipe_2: {
+        name: 'Mousse',
+        preparation_time: '3 hours',
+        servings: '4',
+        img: "/img/recipe2.jpeg",
+        ingredients: [
+          '1 pkg of gelatin',
+          '1 cup of hot water',
+          '1/2 condensed milk',
+          '1 pkg milk cream',
+        ],
+        instructions: [
+          'In the blender, put the boiling water, gelatin, condensed milk and cream.',
+          'Then pour this mixture into a platter, bowl or individual bowls',
+          'Take it to the fridge and leave it for 3 hours to firm up well',
+        ],
+     
+      },
+  }
